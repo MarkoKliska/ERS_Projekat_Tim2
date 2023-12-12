b579dfa5c81a45669059e5585287df84b7a3812b
MarkoKliska <021markokliska@gmail.com>
2023-12-12 22:02:02 +0100

    napravljena osnova za prvu stavku

---
+++ Projekat_Tim2/Klase/UvozOP.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekat_Tim2.Klase
+{
+    internal class UvozOP
+    {
+        private string putanjaUOP;
+
+        public UvozOP()
+        {
+            Console.WriteLine("Unesite apsolutnu putanju do fajla: \n");
+            putanjaUOP = Console.ReadLine();
+        }
+        
+    }
+}
---
+++ Projekat_Tim2/Klase/UvozPP.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekat_Tim2.Klase
+{
+    internal class UvozPP
+    {
+        private string putanjaUPP;
+
+        public UvozPP()
+        {
+            Console.WriteLine("Unesite apsolutnu putanju do fajla: \n");
+            putanjaUPP = Console.ReadLine();            
+        }
+    }
+}
