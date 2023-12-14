cd9004c7696964751f42046270dc5879248b60f1
MarkoKliska <021markokliska@gmail.com>
2023-12-14 19:33:19 +0100

    omogucen uvoz podataka

---
+++ Projekat_Tim2/Klase/InformacijeOU.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekat_Tim2.Klase
+{
+    internal class InformacijeOU
+    {
+        public string imeFajla;
+        public string lokacija;
+        public int sat;
+        public int minut;
+        public int sekunda;
+
+        public InformacijeOU(string filePath)
+        {
+            imeFajla = Path.GetFileName(filePath);
+            lokacija = filePath;
+            DateTime vreme = DateTime.Now;
+            sat = vreme.Hour;
+            minut = vreme.Minute;
+            sekunda = vreme.Second;
+        }
+    }
+}
